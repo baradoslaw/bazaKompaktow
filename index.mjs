d134e73326d8f62ecf1a5d80414b1bac6ff81fdc
RadoslawBaran <baradoslaw@gmail.com>
2024-01-14 18:45:38 +0100

    Add update method to CompactRecord class.

--- index.mjs
+++ index.mjs
@@ -29,3 +29,11 @@ import {CompactRecord} from "./classes/compact.record.mjs";
 //   const cd = await CompactRecord.getOne('99949278-7690-4ab4-9003-bec1c222a1ad');
 //   await cd.delete();
 // })();
+
+// Update
+(async () => {
+  const cd = await CompactRecord.getOne('7b31da88-72e1-4536-8b5c-4cfd0e118814');
+  cd.genre = 'rock';
+  cd.band = 'AC/DC';
+  await cd.update();
+})();
