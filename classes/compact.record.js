a13263b1ad15ba85882696fbd275dd99bba2dda8
RadoslawBaran <baradoslaw@gmail.com>
2024-01-14 17:06:00 +0100

    Add getOne method to CompactRecord class.

--- classes/compact.record.js
+++ classes/compact.record.js
@@ -1,3 +1,5 @@
+import {pool} from "../utils/db";
+
 export class CompactRecord {
   id;
   name;
@@ -13,5 +15,13 @@ export class CompactRecord {
     this.year = obj.year;
     this.genre = obj.genre;
     this.price = obj.price;
+  }
+
+  static async getOne(id) {
+    const [results] = await pool.execute("SELECT * FROM `compacts` where id = :id", {
+      id,
+    });
+
+    return results.length === 0 ? null : new CompactRecord(results[0]);
   }
 }
